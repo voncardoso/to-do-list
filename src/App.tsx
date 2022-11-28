1f3aeb96a912cbe9ff3efbcd5ad1cb9043efbc59
Von Harrison <vonharrison26@gmail.com>
2022-11-28 09:30:28 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+
+
+function App() {
+
+  return (
+    <section>
+      <Header/>
+    </section>
+  )
+}
+
+export default App
